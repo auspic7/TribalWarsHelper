e4e3681e5b3f9257f06db7d86cc2a603e6b9108d
auspic7 <auspic7@gmail.com>
2018-08-05 16:51:52 +0900

    Login and wait for ajax response implemented

--- TribalWarsController.py
+++ TribalWarsController.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from bs4 import BeautifulSoup
@@ -21,12 +22,12 @@ class TribalWarsController:
         world_button.click()
         html = self.driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        username = soup.select('#menu_row > td:nth-of-type(11) > table > tbody > tr:nth-of-type(1) > td > a').text
+        username = soup.select('#menu_row > td:nth-of-type(11) > table > tbody > tr:nth-of-type(1) > td > a')[0].text
 
         if not username:
             return False
         else:
-            return username[0]
+            return username
 
     def login(self, username, password):
         name = self.check_login_session()
@@ -39,7 +40,6 @@ class TribalWarsController:
         self.driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
         self.driver.find_element_by_xpath('//*[@id="login_form"]/div/div/a').click()
 
-        # WebDriverWait(self.driver, 10).until(expected_conditions.)
-        # TODO deal with the immediate refresh after click of login button
+        WebDriverWait(self.driver, 10).until_not(expected_conditions.text_to_be_present_in_element((By.XPATH, '//*[@id="login_form"]/div/div/a'), '...'))
 
         return self.check_login_session()
